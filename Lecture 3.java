555ca1cef4a5275925f5d5e051d767702d5680d4
Rishabh Shukla <rs515666@gmail.com>
2023-06-23 22:20:27 +0530

    Added Lecture 3

---
+++ Lecture 3.java
@@ -0,0 +1,44 @@
+package cipherschools;
+
+class SearchInSortedMatrix
+{
+    static boolean isExist(int matrix[][], int target)
+    {
+        int i = 0;
+        int j = matrix[0].length - 1;
+
+        while (i < matrix.length && j >=0)
+        {
+            int current = matrix[i][j];
+
+            if (current == target) {
+                return true;
+            }
+            if (current > target)
+                j--;
+            else
+                i++;
+        }
+
+        return false;
+    }
+
+
+    public static void main(String args[])
+    {
+        boolean ans;
+
+        int matrix[][] = {
+                          {1, 2, 3, 4}, 
+                          {5, 6, 7, 8},
+                          {9, 10, 11, 12},
+                          {13, 14, 15, 16}
+                         };
+        
+        ans = isExist(matrix, 8);
+        System.out.println(ans);
+
+        ans = isExist(matrix, -1);
+        System.out.println(ans);
+    }
+}
